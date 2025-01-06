4ad2094f3c3e0233a3cbe8fcfb6de5b42f363786
venkateshkanigicherla <153480923+venkateshkanigicherla@users.noreply.github.com>
2025-01-06 08:35:48 +0530

    Add files via upload

---
+++ v2.py
@@ -0,0 +1,6 @@
+a = float(input("enter the value of a."))
+b = float(input("enter the value of b."))
+c = float(input("enter the value of c."))
+x = int(input("enter the value of x."))
+y = a * x**2 + b * x + c 
+print("The predicted value from the input",y,"units.")
